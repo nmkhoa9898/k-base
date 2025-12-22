e7326fa22bcf920ac07eaa1dd2323c7dccb68e34
Khoa - Nguyen Minh <nguyenminhkhoa.nmk98@gmail.com>
2025-12-22 14:36:26 +0700

    spring initialize

---
+++ backend/src/test/java/com/example/kbase_backend/KBaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kbase_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
